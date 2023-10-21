c801d8718e9869e92e17eb2db155b84226da6ce7
Leonid <rockwil91@gmail.com>
2023-10-21 18:03:21 +0300

    решил задачу 18

--- Sem_3/18.py
+++ Sem_3/18.py
@@ -8,3 +8,22 @@
 # 1 2 3 4 5
 # 6
 # -> 5
+
+import random
+
+elValue = int(input("Введите кол-во элементов в массиве:"))
+x = random.randint(1, elValue)
+numList = [random.randint(1, elValue) for i in range(elValue)]
+value = elValue
+
+for i in numList:
+    if x != i and abs(x - i) < value:
+        print(f"if выполнено {i}")
+        value = abs(x - i)
+        number = i
+    print(f"value = {value}")
+    print("переход наследуюзий for")
+
+print(f" x = {x}")
+print(numList)
+print(f"number = {number}")
